e287147f9abd56976b7765e7fadf9a8eef0b458a
Poornima <poornima.godavarthy@gmail.com>
2026-01-05 09:15:39 -0800

    website audit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -259,7 +259,7 @@ export default function ScarlettLabs() {
             lineHeight: 1.2, 
             textAlign: 'center' 
           }}>
-            Where Strategy Meets Intelligence.
+            Intelligence for accessing your organization’s knowledge.
           </h1>
           <p style={{ 
             fontSize: '1rem', 
